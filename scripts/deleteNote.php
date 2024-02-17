218cab3831115816cad67752795b38fc3eb61e57
paolo.brugnola <113890106+PaulXV@users.noreply.github.com>
2024-02-17 14:33:47 +0100

    Revert "crud"
    This reverts commit 389d3e820df8825d7c11064f1e3dc4a0f90543ed.

--- scripts/deleteNote.php
+++ scripts/deleteNote.php
@@ -1,7 +1,1 @@
 <?php
-require 'connessione.php';
-$idNota = $_REQUEST['idNota'];
-$idUser = $_REQUEST['idUser'];
-
-$query = 'DELETE * FROM notes WHERE idUtente == '.$idUser.' AND idNota =='.$idNota;
-$mysqli->close();
