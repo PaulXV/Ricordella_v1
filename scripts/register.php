bfb185ce1780c57d87142d8ca0c501c005b9e78b
paolo.brugnola <113890106+PaulXV@users.noreply.github.com>
2024-02-19 16:33:28 +0100

    small changes

--- scripts/register.php
+++ scripts/register.php
@@ -39,12 +39,12 @@ $conn->close();
 
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">@</span>
-                                <input type="text" name="username" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value="">
+                                <input type="text" name="username" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value="" autocomplete="off">
                             </div>
                             <div class="mb-3">
 
                                 <label for="exampleInputPassword1" class="form-label">Password:</label>
-                                <input type="password" name="password" class="form-control" id="exampleInputPassword1" value="">
+                                <input type="password" name="password" class="form-control" id="exampleInputPassword1" value="" autocomplete="off">
                             </div>
 
                             <button type="submit" class="btn btn-primary">Registrati</button>
