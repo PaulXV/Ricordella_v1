1bec30fe1048a7ab341c82e0b80ec5417d4d3ead
Pietro Margoni <113422244+PietroMargoni@users.noreply.github.com>
2024-03-02 11:20:13 +0100

    Restyling pt3

--- index.php
+++ index.php
@@ -30,24 +30,34 @@ $conn->close();
     <meta charset="UTF-8">
     <title>Le tue note - Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+            user-select: none;
+            background-image: url('./login.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;"
+        }
+    </style>
 </head>
 <body>
 
-<section class="vh-100" style="background: linear-gradient(to left, #A8AEC1, #BFFFBC);">
+<section class="vh-100">
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                 <div class="card shadow-2-strong" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-center">
                         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                            <h3 class="mb-5">Login</h3>
+                            <h3 class="mb-4">Login</h3>
                             
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">@</span>
                                 <input type="text" name="username" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value="" autocomplete="off">
                             </div>
-                            <div class="mb-3">
-
+                            <div class="password-group mb-4">
                                 <label for="exampleInputPassword1" class="form-label">Password:</label>
                                 <input type="password" name="password" class="form-control" id="exampleInputPassword1" value="" autocomplete="off">
                             </div>
